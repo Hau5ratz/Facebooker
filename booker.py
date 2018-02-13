773eba0d46e201afe55d5da50667ba68f489344d
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2018-02-13 12:47:22 -0500

    Update booker.py

--- booker.py
+++ booker.py
@@ -26,7 +26,7 @@ html = html[html.index('[')+2:]
 html = html[:html.index(']')-1]
 x = html.split('","')
 x = [url+x[:-2] for x in x]
-res = requests.head(x[0], allow_redirects=True)
-print res.url
+res = requests.get(x[0], allow_redirects=True)
+print response.history
 
 #print html.index(phrase)
