4eb5251ce7382fb96eaa2a4192cb31ed4a4f0087
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2018-02-13 12:10:28 -0500

    Update booker.py

--- booker.py
+++ booker.py
@@ -9,14 +9,15 @@ browser.set_cookiejar(cookies)
 browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
 browser.set_handle_refresh(False)
 
-url = 'http://www.facebook.com/login.php'
-browser.open(url)
+burl = 'http://www.facebook.com/login.php'
+url = 'https://www.facebook.com/'
+browser.open(burl)
 browser.select_form(nr = 0)       #This is login-password form -> nr = number = 0
 print sys.argv
 browser.form['email'] = sys.argv[1]
 browser.form['pass'] = sys.argv[2]
 browser.submit()
-texty = 'https://www.facebook.com/'+ sys.argv[1].replace(' ','.').lower() + '?redif=8'
+texty = url + sys.argv[1].replace(' ','.').lower() + '?redif=8'
 print texty
 r = browser.open(texty)
 html = r.read()
@@ -25,6 +26,7 @@ html = html[html.index('list:'):]
 html = html[html.index('[')+2:]
 html = html[:html.index(']')-1]
 x = html.split('","')
-x = [x[:-2] for x in x]
+x = [browser.open(url+x[:-2]).geturl() for x in x]
 print x
+
 #print html.index(phrase)
