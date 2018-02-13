cbc430ee2afc48217e52b6ff730208e904cb2fb5
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2018-02-13 11:06:24 -0500

    Update booker.py

--- booker.py
+++ booker.py
@@ -1,8 +1,21 @@
-import requests
-import datetime
+import mechanize
+import sys
 from bs4 import BeautifulSoup as bs
-epoch = str(datetime.datetime.now())
-session = requests.Session()
-result = session.get(self.url % c, headers=headers)
-soup = bs(result.content, 'lxml')
-s =  
+
+phrase = "InitialChatFriendsList"
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+cookies = mechanize.CookieJar()
+browser.set_cookiejar(cookies)
+browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
+browser.set_handle_refresh(False)
+
+url = 'http://www.facebook.com/login.php'
+browser.open(url)
+browser.select_form(nr = 0)       #This is login-password form -> nr = number = 0
+browser.form['email'] = sys.argv[1]
+browser.form['pass'] = sys.argv[2]
+browser.submit()
+r = browser.open('http://facebook.com')
+html = r.read()
+print(html[html.index(phrase):500])
